9f4f8c2317ed735a336cfa7110c327b347483097
Ben Schwennesen <bas65@duke.edu>
2018-11-01 23:17:45 -0400

    Update gitignore to ignore the secret properties

--- src/main/java/com/oasys/tables/Person.java
+++ src/main/java/com/oasys/tables/Person.java
@@ -1,14 +1,24 @@
 package com.oasys.person;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.oasys.hibernate.JsonNodeBinaryType;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+@TypeDef(
+        name = "json-node",
+        typeClass = JsonNodeBinaryType.class
+)
 
 @Data
 @Entity
@@ -16,8 +26,10 @@ import javax.persistence.Table;
 @Setter
 @Table(name = "person")
 public class Person {
-
-    private @Id @GeneratedValue Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "uid")
+    private Long uid;
 
     @Column(unique = true, nullable = false)
     private String email;
@@ -25,14 +37,18 @@ public class Person {
     @Column(nullable = false)
     private String name;
 
+    @Column(name = "graduation_year")
     private int graduationYear;
+
+    @Column(name = "photo_path")
     private String photoPath;
-    // links will be JSON
-    private String links;
+
+    @Type(type = "json-node")
+    private JsonNode links;
 
     public Person() {}
 
-    public Person(String email, String name, int graduationYear, String photoPath, String links) {
+    public Person(String email, String name, int graduationYear, String photoPath, JsonNode links) {
         this.email = email;
         this.name = name;
         this.graduationYear = graduationYear;
