dd23d29b06a257d361e56448d66331d7f43314b2
Alejandro Ortega <ortegaalejandro55@gmail.com>
2018-11-08 12:34:05 -0500

    fixed formatting of loadcsv.sql

--- db/loadcsv.sql
+++ db/loadcsv.sql
@@ -1,17 +1,12 @@
-\copy Person(uid, name, email, graduation_year, photo_path, links, username, password) 
-  FROM 'db/person.csv' DELIMITER ',' CSV;
+\copy Person(uid, name, email, graduation_year, photo_path, links, username, password) FROM 'db/person.csv' DELIMITER ',' CSV;
 
-\copy Flock(gid, name, photo_path)
-  FROM 'db/flock.csv' DELIMITER ',' CSV;
+\copy Flock(gid, name, photo_path) FROM 'db/flock.csv' DELIMITER ',' CSV;
 
-\copy Post(pid, gid, creator_uid, timestamp, kind, body)
-  FROM 'db/post.csv' DELIMITER ',' CSV;
+\copy Post(pid, gid, creator_uid, timestamp, kind, body) FROM 'db/post.csv' DELIMITER ',' CSV;
 
-\copy Comment(cid, pid, creator_uid, timestamp, body)
-  FROM 'db/comment.csv' DELIMITER ',' CSV;
+\copy Comment(cid, pid, creator_uid, timestamp, body) FROM 'db/comment.csv' DELIMITER ',' CSV;
 
-\copy Interest(iid, name, is_study)
-  FROM 'db/interest.csv' DELIMITER ',' CSV;
+\copy Interest(iid, name, is_study) FROM 'db/interest.csv' DELIMITER ',' CSV;
 
 \copy Follows(uid, gid) FROM 'db/follows.csv' DELIMITER ',' CSV;
 
