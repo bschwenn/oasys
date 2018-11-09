8e5f5f54a6ee0c4dbae019d8c4819f3806fc15f7
Ben Schwennesen <bas65@duke.edu>
2018-11-08 22:12:55 -0500

    Add auth to post getting

--- src/main/java/com/oasys/repository/PostRepository.java
+++ src/main/java/com/oasys/repository/PostRepository.java
@@ -6,14 +6,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 @RepositoryRestResource()
 public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
-    @Query("SELECT p FROM Post p WHERE p.gid IN (SELECT f.gid FROM Follows f WHERE f.uid = :uid)")
-    List<Post> getUserFeed(@Param("uid") long uid);
+    @Query( "SELECT post FROM Post post, Person person " +
+            "WHERE person.username = :username " +
+            "AND post.gid IN (SELECT f.gid FROM Follows f WHERE f.uid = person.uid)")
+    @PreAuthorize("#username.equals(authentication.principal)")
+    List<Post> getUserFeed(@Param("username") String username);
 
     @Query("SELECT p FROM Post p WHERE p.gid = :gid")
+    @PreAuthorize("@groupPermissionEvaluator.isInGroup(authentication, #gid)")
     List<Post> getGroupFeed(@Param("gid") long gid);
 }
