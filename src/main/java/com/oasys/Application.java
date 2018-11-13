b8c2c520aae01656a9204cea3a78e0c75f1803c5
Ben Schwennesen <bas65@duke.edu>
2018-11-13 15:25:04 -0500

    Temporary CORS fix

--- src/main/java/com/oasys/Application.java
+++ src/main/java/com/oasys/Application.java
@@ -2,11 +2,26 @@ package com.oasys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    // TODO (Ben): figure out permanent solution to CORS problem
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
